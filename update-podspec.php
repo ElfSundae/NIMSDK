43258dee7a09220f077a12869072afdc176995e9
Elf Sundae <elf.sundae@gmail.com>
2020-11-30 02:57:27 +0800

    Bump 8.1.200

--- update-podspec.php
+++ update-podspec.php
@@ -123,7 +123,7 @@ class UpdatePodspec
     }
 }
 
-$version = '8.1.0';
+$version = '8.1.2';
 $newVersion = null;
 (new UpdatePodspec('NIMSDK', $version, $newVersion))->update();
 (new UpdatePodspec('NIMSDK_LITE', $version, $newVersion))->update();
